0df6a31c91ba9095cc0f1d5f0c6288db6314f77f
Giang Phan <john.phan@gmail.com>
2015-04-23 22:23:00 -0700

    Adding simple scoring heuristics

--- src/waypoints/core.clj
+++ src/waypoints/core.clj
@@ -1,9 +1,6 @@
 (ns waypoints.core
   (:require [clojure.xml :as xml]
-            [clojure.java.io :as io]
-            [clojure.zip :as zip]
-            [clojure.data.zip.xml :as zip-xml]))
-
+            [clojure.java.io :as io]))
 
 (def root (-> "2015.gpx" io/resource io/file xml/parse))
 
@@ -17,16 +14,16 @@
   (->> element (child-node cname) first :content first))
 
 (def waypoints (into {} (for [w (child-node :wpt root)]
-                                [(keyword (child-node-content :name w))
-                                 {:lat  (->> w :attrs :lat read-string)
-                                  :long (->> w :attrs :lon read-string)
-                                  :ele  (read-string (or (child-node-content :ele w) "nil"))
-                                  :pts  (read-string (first (re-seq #"[0-9]+" (child-node-content :desc w))))}])))
+                          [(keyword (child-node-content :name w))
+                           {:lat  (->> w :attrs :lat read-string)
+                            :long (->> w :attrs :lon read-string)
+                            :elevation  (read-string (or (child-node-content :ele w) "nil"))
+                            :points  (read-string (first (re-seq #"[0-9]+" (child-node-content :desc w))))}])))
 
 (defn- deg->radian [deg]
   (Math/toRadians deg))
 
-(defn- distance
+(defn- waypoints->distance
   "Calculates the distance between two waypoints using the haversine formula"
   [w1 w2]
   (let [R 6378100.0 ; Earth's radius in meters
@@ -51,18 +48,50 @@
 (def ignore #{:Start :Finish})
 
 (defn- closest-waypoint
-  ([point] (closest-waypoint point ignore))
-  ([point visited] (let [[n p] point
-                         visited (conj visited n)]
-                     (first (sort-by last <
-                                     (map (fn [[m v]] [m (distance p v)])
-                                          (filter (complement #(contains? visited (first %))) waypoints)))))))
+  ([node] (closest-waypoint node ignore))
+  ([node visited] (let [[n p] node
+                         visited (conj visited n)
+                         not-visited? (complement #(contains? visited (first %)))]
+                     (->> waypoints
+                          (filter not-visited?)
+                          (map (fn [[m v]] [m (waypoints->distance p v)]))
+                          (sort-by last <)
+                          first))))
 
-(defn shortest-path
-  ([node] (shortest-path node ignore []))
-  ([node visited path] (let [closest (closest-waypoint node visited)
-                             closest-key (first closest)
-                             node-key (first node)]
-                         (if (nil? closest)
-                           (conj path :Finish)
-                           (recur (find waypoints closest-key) (conj visited node-key) (conj path node-key))))))
+(defn- find-neighboring-nodes
+  ([node] (find-neighboring-nodes node 5000))
+  ([node threshold] (let [[node-name node-values] node
+                          ignore-nodes (conj ignore node-name)
+                          not-visited? (complement #(contains? ignore-nodes (first %)))]
+                      (->> waypoints
+                           (filter not-visited?)
+                           (map (fn [[n v]] [n (waypoints->distance node-values v)]))
+                           (filter #(< (last %) threshold))
+                           (map first)))))
+
+(defn- score-path
+  "Scores the route between node1 -> node2"
+  [node1 node2]
+  (let [w1 (last node1)
+        w2 (last node2)
+        dist (waypoints->distance w1 w2)
+        ele1 (or (:elevation w1) (:elevation w2) 0)
+        ele2 (or (:elevation w2) (:elevation w1) 0)
+        delta-elev (- ele1 ele2)]
+    (+ (* 0.5 (:points w2)) (* 0.25 dist) (* 0.25 delta-elev))))
+
+(defn best-path [path]
+  "Determines the best path from the last node in the path sequence using the score-path function"
+  (let [last-node-name (last path)
+        node (find waypoints last-node-name)
+        ignore-nodes (into #{} path)
+        not-visited? (complement (fn [[n v]] (contains? ignore-nodes n)))
+        best (->> waypoints
+                  (filter not-visited?)
+                  (map (fn [d] [(first d) (score-path node d)]))
+                  (sort-by last >)
+                  first
+                  first)]
+    (if (nil? best)
+      path
+      (recur (conj path best)))))
